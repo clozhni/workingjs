36f9cf85d235db4aa10405d9609812ad95e8349a
nicholasholtz <nicholas.holtz@va.gov>
2017-03-25 21:39:15 -0400

    It shouldn't be semicolons

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,8 @@ var myImage = document.querySelector('img');
 myImage.onclick = function() {
   var mySrc = myImage.getAttribute('src');
   if(mySrc === 'images/firefox-icon.png') {
-    myImage.setAttribute ('src' ;  'images/ghlogo.png');
+    myImage.setAttribute ('src' ,  'images/ghlogo.png');
   } else {
-    myImage.setAttribute ('src' ; 'images/firefox-icon.png');
+    myImage.setAttribute ('src' , 'images/firefox-icon.png');
   }
 }
